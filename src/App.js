9c22783bca3c4a365075fd1d6c77239bee2930d4
nataliel2514 <natalielopez2514@gmail.com>
2022-06-13 10:09:31 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.scss';
 import Search from './components/Search';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="pet-heading">Avaliable Pets</h1>
       <Search />
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import '../styles/navbar.scss';
+
+function Navbar() {
+    return (
+        <header className="navbar">
+            <h1 className="header-name">Pet Farm</h1>
+            <p className="header-location">Location</p>
+            <p className="header-number">210-000-000</p>
+        </header>
+    )
+}
+
+export default Navbar;
